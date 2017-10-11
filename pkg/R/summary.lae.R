aab6673efa60f505f08a389efc06c28464a3d9a3
bonschomi (Dr. ) <michael.f.schomaker@gmail.com>
2017-10-11 07:42:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mami@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/summary.lae.R
+++
@@ -1,9 +0,0 @@
-summary.lae <- function(object,...){
-
-cat("Summary of LASSO averaging, LASSO and OLS estimation:\n")
-cat("\n")
-printCoefmat(object$coefficients)
-cat("\n")
-if(is.null(object$variable.importance)==FALSE){printCoefmat(object$variable.importance)}
-
-}
