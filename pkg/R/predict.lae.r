2da8524783ad32e174cf0638e0204cfde795b134
bonschomi (Dr. ) <michael.f.schomaker@gmail.com>
2018-08-09 10:10:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mami@196 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/predict.lae.r
+++
@@ -1,7 +0,0 @@
-predict.lae <- function(object,newdata=NULL,clean=T,...){   
-    if(is.null(newdata)){newdata<-data.frame(object$setup[[1]])}
-    if(clean==T){newdata <- newdata[,colnames(newdata)%in%colnames(object$setup[[1]])]}
-    pred <- model.matrix(~.,data=newdata)%*%matrix(object$coefficients[,1],ncol=1)
-    if(object[[6]][[2]]=="binomial"){pred <- plogis(pred)}
-    return(pred)
-}
--- pkg/man/zero_range.rd
+++
@@ -1,5 +0,0 @@
-\name{zero_range}
-\alias{zero_range}
-\title{helper function}
-\description{I'm not in a hurry, I LIVE.}
-\keyword{internal}
