17ce09bc16d63643f67e68840ec41a69d5e3bc31
bonschomi (Dr. ) <michael.f.schomaker@gmail.com>
2019-05-06 10:18:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mami@221 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/predict.mma.r
+++
@@ -1,7 +0,0 @@
-predict.mma <- function(object,newdata=NULL,...){
-    if(is.null(newdata)){newdata<-object$setup[[2]]}
-    newdata <- newdata[,colnames(newdata)%in%colnames(object$setup[[2]])]
-    mf <- paste("~",gsub(".*~","~",object$setup[[1]])[3])
-    pred <- model.matrix(as.formula(mf),data=newdata)%*%matrix(object$coefficients[1,],ncol=1)
-    return(pred)
-}
